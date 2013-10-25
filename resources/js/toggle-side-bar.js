1b0747e7c9c2b12045bd2c47972dc449ceb2b7d3
akiwarheit <akiwarheit@gmail.com>
2013-10-25 21:01:40 +0800

    Modified look and feel

--- resources/js/toggle-side-bar.js
+++ resources/js/toggle-side-bar.js
@@ -4,6 +4,7 @@ var Sidebar = {
     originalWidth : $('.sidebar').width() + "px",
     minimalWidth : $('.sidebar').width() * 0.62 + "px",
     content : $('.content'),
+    container : $('.content > .container'),
     toggle : function(flag) {
         "use strict";
         this.isToggled = (typeof flag !== 'undefined') ? flag : !this.isToggled;
@@ -13,12 +14,14 @@ var Sidebar = {
         if(this.isToggled) {
             this.el.css('width', '-=' + this.minimalWidth);
             this.content.css('padding-left', '-=' + this.minimalWidth);
-            $('.toggler > span').attr('class', 'glyphicon glyphicon-chevron-right');
+            this.container.css('width','100%');
+            $('.toggler > span:first').attr('class', 'glyphicon glyphicon-chevron-right');
             $('.toggle-hidden').hide();
         } else {
             this.el.css('width', this.originalWidth);
             this.content.css('padding-left', '+=' + this.minimalWidth);
-            $('.toggler > span').attr('class', 'glyphicon glyphicon-chevron-left');
+            this.container.css('width','85%');
+            $('.toggler > span:first').attr('class', 'glyphicon glyphicon-chevron-left');
             $('.toggle-hidden').show();
         }
     }
