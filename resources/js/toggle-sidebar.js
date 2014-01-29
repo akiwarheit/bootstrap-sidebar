ab200b62a5b67fe2237d9aefef5cdab7bcc944e7
akiwarheit <akiwarheit@gmail.com>
2014-01-29 20:12:13 +0800

    Added new styling :)

--- resources/js/toggle-sidebar.js
+++ resources/js/toggle-sidebar.js
@@ -1,28 +1,5 @@
-var Sidebar = {
-    isToggled : false,
-    el : $('.sidebar'),
-    originalWidth : $('.sidebar').width() + "px",
-    minimalWidth : $('.sidebar').width() * 0.62 + "px",
-    content : $('.content'),
-    container : $('.content > .container'),
-    toggle : function(flag) {
-        "use strict";
-        this.isToggled = (typeof flag !== 'undefined') ? flag : !this.isToggled;
-        this.adjust();
-    },
-    adjust : function () {
-        if(this.isToggled) {
-            this.el.css('width', '-=' + this.minimalWidth);
-            this.content.css('padding-left', '-=' + this.minimalWidth);
-            this.container.css('width','100%');
-            $('.toggler > span:first').attr('class', 'glyphicon glyphicon-chevron-right');
-            $('.toggle-hidden').hide();
-        } else {
-            this.el.css('width', this.originalWidth);
-            this.content.css('padding-left', '+=' + this.minimalWidth);
-            this.container.css('width','85%');
-            $('.toggler > span:first').attr('class', 'glyphicon glyphicon-chevron-left');
-            $('.toggle-hidden').show();
-        }
-    }
-};
+(function($) {
+    $('.side-menu .items li.has-sub.root-level a').on('click', function () {
+        $(this).parent().find('.sub-items').slideToggle('slow');
+    });
+})(jQuery);
